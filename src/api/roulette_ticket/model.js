a53d84ad9a69900159e59f8425a41a60ef570324
Pratik Raj <pratikraj@Pratiks-MacBook-Air.local>
2025-06-28 12:06:09 +0530

    Added cashType enum to RouletteTicket model

--- src/api/roulette_ticket/model.js
+++ src/api/roulette_ticket/model.js
@@ -1,15 +1,18 @@
 import mongoose, { Schema } from 'mongoose'
 
+const cashTypes = ['REAL', 'VIRTUAL'];
+
 const RouletteTicketSchema = new Schema({
 	user: { type: String, ref: 'User', required: true },
-	roulette: {type: String, ref: 'Roulette', required: true},
+	roulette: { type: String, ref: 'Roulette', required: true },
 	bet: [{
-		blockPlayed: {type: String, required: true},
-		amountPlayed: {type: Number, required: true},
-		amountWon: { type: Number, default: null},		
+		blockPlayed: { type: String, required: true },
+		amountPlayed: { type: Number, required: true },
+		amountWon: { type: Number, default: null },
 	}],
-	totalAmountPlayed: {type: Number, default: null, required: true},
-	totalAmountWon: {type: Number, default: null},
+	cashType: { type: String, enum: cashTypes, required: true },
+	totalAmountPlayed: { type: Number, default: null, required: true },
+	totalAmountWon: { type: Number, default: null },
 }, {
 	timestamps: true,
 	toJSON: {
